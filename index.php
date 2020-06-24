0c57e6b9690c037163276e548357ce232cdbc49c
capoaira <capoaira@web.de>
2020-06-24 15:04:07 +0200

    Struktur

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Titel</title>
+		<link rel="icon" href="img/favicon.png" type="image/png">
+		<link rel="apple-touch-icon" href="touch-icon-iphone.png">
+		<link rel="mask-icon" href="mask-icon.svg" color="#000">
+		<link rel="stylesheet" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+	</head>
+	<body>
+		
+	</body>
+</html>
