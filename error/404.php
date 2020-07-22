dbacb73dc1c6e89335f4e3520cb2f66380bbd8b0
capoaira <capoaira@web.de>
2020-07-22 20:59:32 +0200

    Struktur und Error Pages
    #11

---
+++ error/404.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Seite nicht gefunden</title>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+	</head>
+	<body>
+		<p style="width:100%; font-family:Roboto; font-size:1em; text-align:center;"><span style="font-size:4em;">Error 404</span><br>
+		Die angeforderte Seite konnte leider nicht gefunden werden.<br>
+		<a href="/P5/index.php" title="Startseite">Hier</a> ist unsere Startseite</p>
+	</body>
+</html>
